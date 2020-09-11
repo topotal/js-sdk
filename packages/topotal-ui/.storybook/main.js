dd432b324eaa3f946770c137d1a509d9efec0a82
sawa-zen <sawadasuiren@gmail.com>
2020-09-11 17:08:55 +0900

    chore: update packages

--- packages/topotal-ui/.storybook/main.js
+++ packages/topotal-ui/.storybook/main.js
@@ -1,8 +1,5 @@
-const path = require('path')
-
 module.exports = {
   stories: ['../src/**/*.stories.tsx'],
-  addons: ['@storybook/preset-typescript'],
   webpackFinal: async config => {
     config.resolve.alias['react-native'] = require.resolve('react-native-web')
     return config
