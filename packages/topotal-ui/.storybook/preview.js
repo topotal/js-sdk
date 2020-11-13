d1948158ef99783ddfc51dce6cb583dedce44d2d
sawa-zen <sawadasuiren@gmail.com>
2020-11-13 14:53:10 +0900

    feat: Add storybook doc

--- packages/topotal-ui/.storybook/preview.js
+++ packages/topotal-ui/.storybook/preview.js
@@ -3,6 +3,10 @@ import { addDecorator } from '@storybook/react'
 import { VStack } from '../src/components'
 import { ThemeContext, defaultTheme } from '../src/theme'
 
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+}
+
 addDecorator(storyFn => (
   <ThemeContext.Provider value={defaultTheme}>
     <VStack gap={16} style={{ padding: 24 }}>
