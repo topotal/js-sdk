fa5e0c736b05f263982c342946ad52162045ad2e
sawa-zen <sawadasuiren@gmail.com>
2021-01-09 01:10:55 +0900

    Revert "fix: Patch dropdown bug"
    This reverts commit 386373f313bafd3265f1058f2e255d4304b9135d.

--- packages/topotal-ui/src/components/Dropdown/index.stories.tsx
+++ packages/topotal-ui/src/components/Dropdown/index.stories.tsx
@@ -2,22 +2,22 @@ import React, { useState } from 'react'
 import Button from '../Button'
 import VStack from '../VStack'
 import Text from '../Text'
-import DropdownWeb from '.'
+import Dropdown from '.'
 
 export default {
-  title: 'components/DropdownWeb',
+  title: 'components/Dropdown',
 }
 
 export const Default = () => {
   const [visible, setVisible] = useState(false)
   return (
-    <VStack style={{ width: 300, height: 2500, paddingTop: 200 }}>
+    <VStack style={{ width: 300 }}>
       <Button title="Show Dropdown" onPress={() => { setVisible(true) }}/>
-      <DropdownWeb visible={visible} onPressBackground={() => { setVisible(false) }}>
+      <Dropdown visible={visible} onPressBackground={() => { setVisible(false) }}>
         <VStack style={{ padding: 24 }}>
           <Text>Dropdown Children</Text>
         </VStack>
-      </DropdownWeb>
+      </Dropdown>
     </VStack>
   )
 }
