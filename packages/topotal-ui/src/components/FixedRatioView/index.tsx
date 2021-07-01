72faf2991bcdeadfa8961e2eba1862546faa300f
sawa-zen <sawadasuiren@gmail.com>
2021-07-01 11:53:13 +0900

    feat(topotal-ui): Add FixedRatioView

---
+++ packages/topotal-ui/src/components/FixedRatioView/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { StyleProp, View, ViewStyle } from 'react-native'
+import { VStack } from '..'
+
+interface Props {
+  style?: StyleProp<ViewStyle>
+  ratio: [number, number]
+}
+
+export const FixedRatioView: React.FC<Props> = ({
+  style,
+  ratio,
+  children,
+}) => {
+  const [xRatio, yRatio] = ratio
+
+  return (
+    <VStack
+      style={[{
+        width: '100%',
+        position: 'relative',
+      }, style]}
+    >
+      <View
+        style={{
+          paddingTop: `${yRatio / xRatio * 100}%`,
+        }}
+      />
+      <View
+        style={{
+          position: 'absolute',
+          width: '100%',
+          height: '100%',
+        }}
+      >
+        {children}
+      </View>
+    </VStack>
+  )
+}
