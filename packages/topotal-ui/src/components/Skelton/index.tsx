11436469cacfda26fe075e7fcd18944c3e279226
sawa-zen <sawadasuiren@gmail.com>
2021-07-18 12:28:33 +0900

    feat: Add Skelton

---
+++ packages/topotal-ui/src/components/Skelton/index.tsx
@@ -0,0 +1,41 @@
+import React, { memo, useEffect, useRef } from 'react'
+import { Animated, StyleProp, ViewStyle } from 'react-native'
+import { useTheme } from '@topotal/topotal-ui'
+
+interface Props {
+  style?: StyleProp<ViewStyle>
+}
+
+export const Skelton = memo<Props>(({ style }) => {
+  const fadeAnim = useRef(new Animated.Value(0.2)).current
+  const { color } = useTheme()
+
+  useEffect(() => {
+    const anime = Animated.loop(
+      Animated.sequence([
+        Animated.timing(fadeAnim, {
+          toValue: 1,
+          duration: 750,
+          useNativeDriver: false,
+        }),
+        Animated.timing(fadeAnim, {
+          toValue: 0.2,
+          duration: 750,
+          useNativeDriver: false,
+        }),
+      ]),
+    )
+    anime.start()
+  }, [fadeAnim])
+
+  return (
+    <Animated.View
+      style={[{
+        width: 100,
+        height: 100,
+        backgroundColor: color.secandaryTextDark,
+        opacity: fadeAnim,
+      }, style]}
+    />
+  )
+})
