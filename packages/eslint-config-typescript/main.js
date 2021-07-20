2d6eda4623cffa82dd59e5a71dd750e0dfce597d
sawa-zen <sawadasuiren@gmail.com>
2021-07-20 13:35:13 +0900

    chore: Add indent reset rule to eslint-config-typescript

--- packages/eslint-config-typescript/main.js
+++ packages/eslint-config-typescript/main.js
@@ -7,9 +7,11 @@ module.exports = {
     "@typescript-eslint",
   ],
   rules: {
-    "@typescript-eslint/indent": ["error", 2, {"SwitchCase": 1}],
     "@typescript-eslint/consistent-type-definitions": ["error", "interface"],
     "@typescript-eslint/explicit-module-boundary-types": "off",
+    // Indent setting
+    "indent": "off",
+    "@typescript-eslint/indent": ["error", 2, {"SwitchCase": 1}],
     // Disallow unused variables
     "no-unused-vars": "off",
     "@typescript-eslint/no-unused-vars": [
