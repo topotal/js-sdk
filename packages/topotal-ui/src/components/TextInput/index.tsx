639fd05287a2801aa50697a5dc1c8b87303bc00c
mtmtkzm <mtmtkzm00@gmail.com>
2021-10-22 21:50:44 +0900

    add: add prop 'disabled' into TextInput

--- packages/topotal-ui/src/components/TextInput/index.tsx
+++ packages/topotal-ui/src/components/TextInput/index.tsx
@@ -1,6 +1,6 @@
 import { memo, Ref } from 'react'
 import { TextInput as BaseInput } from 'react-native'
-import { useFocus, useInputValue } from '../../hooks'
+import { useFocus, useFocusOutlineStyle, useInputValue } from '../../hooks'
 import type { InputFrameSize } from '../InputFrame'
 import { InputFrame } from '../InputFrame'
 import { Icon } from '..'
@@ -10,6 +10,7 @@ type BaseProps = {
   innerRef?: Ref<BaseInput>
   error?: boolean
   size?: InputFrameSize
+  disabled?: boolean
   startIconName?: string
 } & Omit<React.ComponentProps<typeof BaseInput>, 'multiline'>
 
@@ -21,6 +22,7 @@ export const TextInput = memo<Props>(({
   autoCapitalize = 'none',
   error = false,
   size = 'large',
+  disabled = false,
   startIconName,
   innerRef,
   style,
@@ -33,10 +35,11 @@ export const TextInput = memo<Props>(({
   })
   const { isFocused, handleFocus, handleBlur } = useFocus()
   const { styles } = useStyles()
+  const { styles: focusOutlineStyles } = useFocusOutlineStyle({ focus: isFocused, disabledPointerEvents: disabled })
 
   return (
     <InputFrame
-      style={style}
+      style={[style, focusOutlineStyles.wrapper]}
       size={size}
       error={error}
       focus={isFocused}
@@ -51,6 +54,7 @@ export const TextInput = memo<Props>(({
       renderInput={({ style }) => (
         <BaseInput
           {...rest}
+          focusable={!disabled}
           value={innerValue}
           autoCapitalize={autoCapitalize}
           multiline={false}
