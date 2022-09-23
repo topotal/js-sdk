5a014afc2fddd1eaa72e8f4d6de183486e76149b
sawa-zen <sawadasuiren@gmail.com>
2022-09-24 03:00:57 +0900

    feat: Add InputField

---
+++ packages/topotal-ui/src/components/InputField/index.stories.tsx
@@ -0,0 +1,18 @@
+import { View } from 'react-native'
+import { Text } from '../Text'
+import { InputField } from '.'
+
+export default {
+  title: 'components/InputField',
+}
+
+export const Default = () => (
+  <InputField
+    title="Title"
+    errorMessage="Error Message"
+  >
+    <View style={{ borderWidth: 1, width: 300 }}>
+      <Text>Child</Text>
+    </View>
+  </InputField>
+)
---
+++ packages/topotal-ui/src/components/InputField/index.tsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import { StyleProp, ViewStyle } from 'react-native'
+import { HStack, Text, useTheme, VStack } from '@topotal/topotal-ui'
+
+interface Props {
+  style?: StyleProp<ViewStyle>
+  children?: React.ReactNode
+  title?: string
+  description?: string
+  optional?: boolean
+  errorMessage?: string
+}
+
+export const InputField = ({
+  style,
+  children,
+  title,
+  description,
+  optional,
+  errorMessage,
+}: Props): JSX.Element => {
+  const { color } = useTheme()
+
+  return (
+    <VStack style={style} gap={8}>
+      <VStack>
+        <HStack gap={8} align="center">
+          {title ? (
+            <Text weight="bold">
+              {title}
+            </Text>
+          ) : null}
+          {optional ? (
+            <Text
+              type="small_body"
+              style={{ color: color.secondaryTextDark }}
+            >
+              (Optional)
+            </Text>
+          ) : null}
+        </HStack>
+        {description ? (
+          <Text type="small_body">
+            {description}
+          </Text>
+        ) : null}
+      </VStack>
+      <VStack>
+        {children}
+      </VStack>
+      {errorMessage ? (
+        <Text
+          type="small_body"
+          style={{ color: color.error }}
+        >
+          {errorMessage}
+        </Text>
+      ) : null}
+    </VStack>
+  )
+}
