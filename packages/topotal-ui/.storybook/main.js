a4a9cdb0b801519a1d49a2c87d75bf8bb7e53d31
Takayoshi Sawada <sawadasuiren@gmail.com>
2022-12-22 16:53:20 +0900

    fix: storybook settings (#340)

--- packages/topotal-ui/.storybook/main.js
+++ packages/topotal-ui/.storybook/main.js
@@ -1,11 +1,15 @@
 module.exports = {
+  staticDirs: [
+    '../assets',
+  ],
   stories: [
     '../src/**/*.stories.mdx',
     '../src/**/*.stories.tsx',
   ],
   addons: [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
+    "@storybook/addon-essentials",
+    "@storybook/addon-actions"
   ],
   webpackFinal: async config => {
     config.resolve.alias['@react-native-picker/picker'] = require.resolve('react-native-web')
