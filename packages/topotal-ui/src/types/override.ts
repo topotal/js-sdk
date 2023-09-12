3f9e564be77bf6911e5c41f5208e4228b03d5fad
nariakiraHara <nariakra@gmail.com>
2023-09-12 21:26:13 +0900

    delete pressable props

--- packages/topotal-ui/src/types/override.ts
+++ packages/topotal-ui/src/types/override.ts
@@ -1,13 +1,6 @@
-import { MouseEvent } from 'react-native'
-
 declare module 'react-native' {
   interface PressableStateCallbackType {
     hovered?: boolean
-  }
-
-  interface PressableProps {
-    onHoverIn?: null | ((event: MouseEvent) => void) | undefined;
-    onHoverOut?: null | ((event: MouseEvent) => void) | undefined;
   }
 
   interface PickerProps {
