0b131c807511b5ed03e799dfcb2340676595211b
nariakira-hara <nariakira.hara@linecorp.com>
2024-01-28 13:29:04 +0900

    add config settings

--- packages/topotal-ui/vite.config.ts
+++ packages/topotal-ui/vite.config.ts
@@ -1,10 +1,30 @@
 import react from '@vitejs/plugin-react'
+import path from 'path'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 // eslint-disable-next-line
 export default defineConfig({
   plugins: [react()],
+  build: {
+    emptyOutDir: false,
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'), // ライブラリのエントリーポイント
+      name: 'topotalUI',
+      formats: ['umd', 'es', 'cjs'],
+    },
+    outDir: 'dist/esm', // ビルドしたファイルの出力先ディレクトリ
+    assetsDir: 'assets',
+    sourcemap: true,
+    rollupOptions: {
+      external: ['react'],
+      output: {
+        globals: {
+          react: 'React',
+        },
+      },
+    },
+  },
   resolve: {
     alias: {
       '@react-native-picker/picker': 'react-native-web',
