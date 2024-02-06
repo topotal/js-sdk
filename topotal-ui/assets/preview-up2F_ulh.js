85f8df158f2c615cf2e21de83c663b6e0a7f3a5f
nariakiraHara <nariakiraHara@users.noreply.github.com>
2024-02-06 10:41:32 +0000

    deploy: 88485872658fdce0c51f7e434f87676fa5cc9682

---
+++ topotal-ui/assets/preview-up2F_ulh.js
@@ -0,0 +1,7 @@
+import{_ as t}from"./iframe-Q2VdwUlW.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-6OEUo4RG.js").then(e=>e.D),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10]),import.meta.url);return new r}}};export{_ as parameters};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-6OEUo4RG.js","./iframe-Q2VdwUlW.js","./index-4g5l5LRQ.js","./_commonjsHelpers-4gQjN7DL.js","./react-18-ba7OOUbL.js","./index-jmm5gWkb.js","./index-ogXoivrg.js","./_getPrototype-MXemwaK5.js","./_baseIsEqual-CFr8iVHd.js","./uniq-DjaUhnlG.js","./index-PPLHz8o0.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
