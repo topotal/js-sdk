6041ba078a38590c9428a2506437c661c2f5bd18
sawa-zen <sawa-zen@users.noreply.github.com>
2024-02-29 08:04:32 +0000

    deploy: e399757b98c7d63b8dc90377f2c33d950b2ce5cc

---
+++ topotal-ui/assets/index.stories-_B0MCdlF.js
@@ -0,0 +1,8 @@
+import{j as e,V as s}from"./index-FBWzmZge.js";import{T as p}from"./index-qmPSAIhr.js";import{a as n}from"./index-0QsIm73i.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./index-B0s6Peb0.js";import"./index-2q6owiuR.js";import"./index-LgXL2HH1.js";import"./index-MT7WmrMi.js";import"./index-Ed39mVia.js";import"./index-aKRnNsEw.js";import"./index-jmm5gWkb.js";import"./index-0tTWSQQD.js";import"./index-maDx5jV0.js";const F={title:"components/InputField"},r=()=>e.jsx(n,{title:"Title",description:"これは説明です。",errorMessage:"Error Message",children:e.jsx(s,{style:{borderWidth:1,width:300},children:e.jsx(p,{children:"Child"})})});var t,i,o;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`() => <InputField title="Title" description="これは説明です。" errorMessage="Error Message">
+    <View style={{
+    borderWidth: 1,
+    width: 300
+  }}>
+      <Text>Child</Text>
+    </View>
+  </InputField>`,...(o=(i=r.parameters)==null?void 0:i.docs)==null?void 0:o.source}}};const C=["Default"];export{r as Default,C as __namedExportsOrder,F as default};
