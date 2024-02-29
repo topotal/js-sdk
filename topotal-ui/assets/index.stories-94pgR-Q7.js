da1e07b75c3ac8d27dc43dd6ebbfb35403d2c14c
sawa-zen <sawa-zen@users.noreply.github.com>
2024-02-29 08:06:25 +0000

    deploy: a70227d7d1b0539a5cc522c7ef273c6683c345b1

---
+++ topotal-ui/assets/index.stories-94pgR-Q7.js
@@ -0,0 +1,18 @@
+import{j as e}from"./index-FBWzmZge.js";import{g as s}from"./index-BFkYI3Bt.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./index-LgXL2HH1.js";import"./index-B0s6Peb0.js";import"./index-MT7WmrMi.js";import"./index-Ed39mVia.js";import"./index-aKRnNsEw.js";import"./index-2q6owiuR.js";import"./index-7jojP3g7.js";import"./index-jmm5gWkb.js";import"./index-0tTWSQQD.js";import"./index-maDx5jV0.js";const z={title:"components/TextInput"},o=()=>e.jsxs(e.Fragment,{children:[e.jsx(s,{}),e.jsx(s,{startIconName:"search",placeholder:"Search"}),e.jsx(s,{placeholder:"Sample Placeholder"}),e.jsx(s,{secureTextEntry:!0,placeholder:"Password"}),e.jsx(s,{error:!0}),e.jsx(s,{value:"default value",onChangeText:n=>console.info(n)}),e.jsx(s,{value:"disabled",disabled:!0}),e.jsx(s,{size:"medium"}),e.jsx(s,{size:"medium",startIconName:"search",placeholder:"Search"}),e.jsx(s,{size:"medium",placeholder:"Sample Placeholder"}),e.jsx(s,{size:"medium",secureTextEntry:!0,placeholder:"Password"}),e.jsx(s,{size:"medium",error:!0}),e.jsx(s,{size:"medium",value:"default value",onChangeText:n=>console.info(n)}),e.jsx(s,{size:"medium",value:"press some kind of key",onKeyPress:n=>console.info(n.nativeEvent.key)}),e.jsx(s,{size:"medium",value:"press command + enter",onCmdEnterPress:()=>console.log("command and enter pressed")}),e.jsx(s,{size:"medium",value:"press some kind of key or command + enter",onKeyPress:n=>console.info(n.nativeEvent.key),onCmdEnterPress:()=>console.log("command and enter pressed")})]});var r,t,a;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`() => <>
+    <TextInput />
+    <TextInput startIconName="search" placeholder="Search" />
+    <TextInput placeholder="Sample Placeholder" />
+    <TextInput secureTextEntry placeholder="Password" />
+    <TextInput error />
+    <TextInput value="default value" onChangeText={value => console.info(value)} />
+    <TextInput value="disabled" disabled />
+    <TextInput size="medium" />
+    <TextInput size="medium" startIconName="search" placeholder="Search" />
+    <TextInput size="medium" placeholder="Sample Placeholder" />
+    <TextInput size="medium" secureTextEntry placeholder="Password" />
+    <TextInput size="medium" error />
+    <TextInput size="medium" value="default value" onChangeText={value => console.info(value)} />
+    <TextInput size="medium" value="press some kind of key" onKeyPress={event => console.info(event.nativeEvent.key)} />
+    <TextInput size="medium" value="press command + enter" onCmdEnterPress={() => console.log('command and enter pressed')} />
+    <TextInput size="medium" value="press some kind of key or command + enter" onKeyPress={event => console.info(event.nativeEvent.key)} onCmdEnterPress={() => console.log('command and enter pressed')} />
+  </>`,...(a=(t=o.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};const y=["all"];export{y as __namedExportsOrder,o as all,z as default};
