563bb44ad40af4efce651c6283f0351689b8d0dc
sawa-zen <sawa-zen@users.noreply.github.com>
2024-03-19 13:14:53 +0000

    deploy: 4464033f2d7c59aedc8aa010d895a774fea0001e

---
+++ topotal-ui/assets/index.stories-Bb3_zWih.js
@@ -0,0 +1,21 @@
+import{j as e,V as i}from"./index-FBWzmZge.js";import{d as n}from"./index-B0s6Peb0.js";import{C as o,S as l}from"./index-TDLNL_Kr.js";import{V as c,T as t}from"./index-7jojP3g7.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./index-LgXL2HH1.js";import"./index-MT7WmrMi.js";import"./index-Ed39mVia.js";import"./index-aKRnNsEw.js";import"./index-2q6owiuR.js";import"./index-jmm5gWkb.js";import"./index-0tTWSQQD.js";import"./index-maDx5jV0.js";const V={title:"components/Card"},r=()=>e.jsxs(c,{gap:16,style:{backgroundColor:n.color.background,padding:24},children:[e.jsx(o,{level:1,header:e.jsx(t,{weight:"bold",children:"header"}),footer:e.jsxs(e.Fragment,{children:[e.jsx(t,{children:"left"}),e.jsx(l,{}),e.jsx(t,{children:"right"})]}),children:e.jsx(t,{children:"test"})}),e.jsxs(o,{level:2,header:e.jsx(t,{weight:"bold",children:"header"}),footer:e.jsx(t,{children:"footer"}),children:[e.jsx(t,{children:"test"}),e.jsx(i,{style:{width:100,height:100,backgroundColor:"red",position:"absolute"}})]})]});var s,a,d;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`() => <VStack gap={16} style={{
+  backgroundColor: defaultTheme.color.background,
+  padding: 24
+}}>
+    <Card level={1} header={<Text weight="bold">header</Text>} footer={<>
+          <Text>left</Text>
+          <Spacer />
+          <Text>right</Text>
+        </>}>
+      <Text>test</Text>
+    </Card>
+    <Card level={2} header={<Text weight="bold">header</Text>} footer={<Text>footer</Text>}>
+      <Text>test</Text>
+      <View style={{
+      width: 100,
+      height: 100,
+      backgroundColor: 'red',
+      position: 'absolute'
+    }} />
+    </Card>
+  </VStack>`,...(d=(a=r.parameters)==null?void 0:a.docs)==null?void 0:d.source}}};const v=["List"];export{r as List,v as __namedExportsOrder,V as default};
