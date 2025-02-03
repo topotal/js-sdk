b7b2ff1cf12cf6e7f9650fcbb57e52e86f2b99d7
sawa-zen <sawa-zen@users.noreply.github.com>
2025-02-03 08:50:03 +0000

    deploy: a7bb3671a9afa7d4126bf4f1e23e2feabeccf879

---
+++ topotal-ui/assets/index.stories-exfznpec.js
@@ -0,0 +1,21 @@
+import{j as e,V as i}from"./index-CWlY1mBF.js";import{d as n}from"./index-BNZ4CFqE.js";import{C as o,S as l}from"./index-DoLVjI-J.js";import{V as c,T as t}from"./index-BHSnVWMv.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-BScxPdS3.js";import"./extends-CCbyfPlC.js";import"./index-hJCXZwW-.js";import"./index-CWv6xqnD.js";import"./index-C-DQKNxf.js";import"./index-Ithvx5T6.js";import"./index-BtM5VmRH.js";import"./index-BK3wfexn.js";import"./index-C48Letuc.js";const v={title:"components/Card"},r=()=>e.jsxs(c,{gap:16,style:{backgroundColor:n.color.background,padding:24},children:[e.jsx(o,{level:1,header:e.jsx(t,{weight:"bold",children:"header"}),footer:e.jsxs(e.Fragment,{children:[e.jsx(t,{children:"left"}),e.jsx(l,{}),e.jsx(t,{children:"right"})]}),children:e.jsx(t,{children:"test"})}),e.jsxs(o,{level:2,header:e.jsx(t,{weight:"bold",children:"header"}),footer:e.jsx(t,{children:"footer"}),children:[e.jsx(t,{children:"test"}),e.jsx(i,{style:{width:100,height:100,backgroundColor:"red",position:"absolute"}})]})]});var s,a,d;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`() => <VStack gap={16} style={{
+  backgroundColor: defaultTheme.color.background,
+  padding: 24
+}}>
+    <Card level={1} header={<Text weight="bold">header</Text>} footer={<>
+          <Text>left</Text>
+          <Spacer />
+          <Text>right</Text>
+        </>}>
+      <Text>test</Text>
+    </Card>
+    <Card level={2} header={<Text weight="bold">header</Text>} footer={<Text>footer</Text>}>
+      <Text>test</Text>
+      <View style={{
+      width: 100,
+      height: 100,
+      backgroundColor: 'red',
+      position: 'absolute'
+    }} />
+    </Card>
+  </VStack>`,...(d=(a=r.parameters)==null?void 0:a.docs)==null?void 0:d.source}}};const y=["List"];export{r as List,y as __namedExportsOrder,v as default};
