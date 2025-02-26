872a983af81c332750c9382ed3a9a85341eccda5
sawa-zen <sawa-zen@users.noreply.github.com>
2025-02-26 05:05:39 +0000

    deploy: 381d25adf7573d1386ed701783e2e0293ed6f5b9

---
+++ topotal-ui/assets/index.stories-D5R7K7Aj.js
@@ -0,0 +1,18 @@
+import{j as e}from"./index-C19Z7ebk.js";import{g as s}from"./index-qG8QhJRO.js";import"./index-BS1Ahyye.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-bQVzNpnV.js";import"./index-DVaO3OZA.js";import"./extends-CCbyfPlC.js";import"./index-C-ccZ4uZ.js";import"./index-CXOEo_ad.js";import"./index-B-tRbuPw.js";import"./index-B7E4H2Lj.js";import"./index-Do7bR3wk.js";import"./index-hvGZvQJJ.js";import"./index-BWnoVj4o.js";import"./index-CLPokHI7.js";const y={title:"components/TextInput"},o=()=>e.jsxs(e.Fragment,{children:[e.jsx(s,{}),e.jsx(s,{startIconName:"search",placeholder:"Search"}),e.jsx(s,{placeholder:"Sample Placeholder"}),e.jsx(s,{secureTextEntry:!0,placeholder:"Password"}),e.jsx(s,{error:!0}),e.jsx(s,{value:"default value",onChangeText:n=>console.info(n)}),e.jsx(s,{value:"disabled",disabled:!0}),e.jsx(s,{size:"medium"}),e.jsx(s,{size:"medium",startIconName:"search",placeholder:"Search"}),e.jsx(s,{size:"medium",placeholder:"Sample Placeholder"}),e.jsx(s,{size:"medium",secureTextEntry:!0,placeholder:"Password"}),e.jsx(s,{size:"medium",error:!0}),e.jsx(s,{size:"medium",value:"default value",onChangeText:n=>console.info(n)}),e.jsx(s,{size:"medium",value:"press some kind of key",onKeyPress:n=>console.info(n.nativeEvent.key)}),e.jsx(s,{size:"medium",value:"press command + enter",onCmdEnterPress:()=>console.log("command and enter pressed")}),e.jsx(s,{size:"medium",value:"press some kind of key or command + enter",onKeyPress:n=>console.info(n.nativeEvent.key),onCmdEnterPress:()=>console.log("command and enter pressed")})]});var r,t,a;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`() => <>
+    <TextInput />
+    <TextInput startIconName="search" placeholder="Search" />
+    <TextInput placeholder="Sample Placeholder" />
+    <TextInput secureTextEntry placeholder="Password" />
+    <TextInput error />
+    <TextInput value="default value" onChangeText={value => console.info(value)} />
+    <TextInput value="disabled" disabled />
+    <TextInput size="medium" />
+    <TextInput size="medium" startIconName="search" placeholder="Search" />
+    <TextInput size="medium" placeholder="Sample Placeholder" />
+    <TextInput size="medium" secureTextEntry placeholder="Password" />
+    <TextInput size="medium" error />
+    <TextInput size="medium" value="default value" onChangeText={value => console.info(value)} />
+    <TextInput size="medium" value="press some kind of key" onKeyPress={event => console.info(event.nativeEvent.key)} />
+    <TextInput size="medium" value="press command + enter" onCmdEnterPress={() => console.log('command and enter pressed')} />
+    <TextInput size="medium" value="press some kind of key or command + enter" onKeyPress={event => console.info(event.nativeEvent.key)} onCmdEnterPress={() => console.log('command and enter pressed')} />
+  </>`,...(a=(t=o.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};const P=["all"];export{P as __namedExportsOrder,o as all,y as default};
--- topotal-ui/assets/index.stories-ZfeSD2um.js
+++
@@ -1,8 +0,0 @@
-import{j as e,V as s}from"./index-C19Z7ebk.js";import{T as p}from"./index-BLN48eW_.js";import{a as m}from"./index-1KiLIJGm.js";import"./index-BS1Ahyye.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-DVaO3OZA.js";import"./index-B7E4H2Lj.js";import"./index-bQVzNpnV.js";import"./extends-CCbyfPlC.js";import"./index-C-ccZ4uZ.js";import"./index-CXOEo_ad.js";import"./index-B-tRbuPw.js";import"./index-hvGZvQJJ.js";import"./index-BWnoVj4o.js";import"./index-CLPokHI7.js";const C={title:"components/InputField"},r=()=>e.jsx(m,{title:"Title",description:"これは説明です。",errorMessage:"Error Message",children:e.jsx(s,{style:{borderWidth:1,width:300},children:e.jsx(p,{children:"Child"})})});var t,i,o;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`() => <InputField title="Title" description="これは説明です。" errorMessage="Error Message">
-    <View style={{
-    borderWidth: 1,
-    width: 300
-  }}>
-      <Text>Child</Text>
-    </View>
-  </InputField>`,...(o=(i=r.parameters)==null?void 0:i.docs)==null?void 0:o.source}}};const I=["Default"];export{r as Default,I as __namedExportsOrder,C as default};
