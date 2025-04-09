13b38240e6f57b0a881b71a8cd673fb8eb810b12
sawa-zen <sawa-zen@users.noreply.github.com>
2025-04-09 01:26:10 +0000

    deploy: e8b1db88de17d09553c1c3b3cfc07e4153e5e27d

--- topotal-ui/assets/index.stories-XM9cpG8t.js
+++
@@ -1,8 +0,0 @@
-import{j as e,V as s}from"./index-CsMwEyaY.js";import{T as p}from"./index-DJ-hCCZj.js";import{a as m}from"./index-D4LsY2JU.js";import"./index-BS1Ahyye.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-DVaO3OZA.js";import"./index-DzwhhsDO.js";import"./index-C5M_XDvz.js";import"./extends-CF3RwP-h.js";import"./index-DsB9b_Jd.js";import"./index-CotB3pBB.js";import"./index-B5Yd_gvL.js";import"./index-hvGZvQJJ.js";import"./index-CmD9pS_U.js";import"./index-D2GE82NR.js";const C={title:"components/InputField"},r=()=>e.jsx(m,{title:"Title",description:"これは説明です。",errorMessage:"Error Message",children:e.jsx(s,{style:{borderWidth:1,width:300},children:e.jsx(p,{children:"Child"})})});var t,i,o;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`() => <InputField title="Title" description="これは説明です。" errorMessage="Error Message">
-    <View style={{
-    borderWidth: 1,
-    width: 300
-  }}>
-      <Text>Child</Text>
-    </View>
-  </InputField>`,...(o=(i=r.parameters)==null?void 0:i.docs)==null?void 0:o.source}}};const I=["Default"];export{r as Default,I as __namedExportsOrder,C as default};
---
+++ topotal-ui/assets/index.stories-uCLNQxEj.js
@@ -0,0 +1,11 @@
+import{j as e}from"./index-CsMwEyaY.js";import"./index-DJ-hCCZj.js";import{H as m,I as t}from"./index-BQ5CWzQ2.js";import"./index-CmD9pS_U.js";import"./index-C5M_XDvz.js";import"./index-D2GE82NR.js";import"./index-BS1Ahyye.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-DVaO3OZA.js";import"./index-DzwhhsDO.js";import"./index-DsB9b_Jd.js";import"./extends-CF3RwP-h.js";import"./index-hvGZvQJJ.js";import"./index-B5Yd_gvL.js";import"./index-CotB3pBB.js";const h={title:"components/Icon"},o={borderWidth:1,borderColor:"black",tintColor:"red"},n=()=>e.jsxs(m,{gap:16,children:[e.jsx(t,{name:"done",style:o}),e.jsx(t,{name:"settings-black",style:o}),e.jsx(t,{name:"whatshot",style:o}),e.jsx(t,{name:"group",style:o}),e.jsx(t,{name:"memory",style:o}),e.jsx(t,{name:"book",style:o}),e.jsx(t,{name:"credit_card",style:o}),e.jsx(t,{name:"notifications_none",style:o}),e.jsx(t,{name:"domain",style:o})]});var s,a,r;n.parameters={...n.parameters,docs:{...(s=n.parameters)==null?void 0:s.docs,source:{originalSource:`() => <HStack gap={16}>
+    <Icon name="done" style={style} />
+    <Icon name="settings-black" style={style} />
+    <Icon name="whatshot" style={style} />
+    <Icon name="group" style={style} />
+    <Icon name="memory" style={style} />
+    <Icon name="book" style={style} />
+    <Icon name="credit_card" style={style} />
+    <Icon name="notifications_none" style={style} />
+    <Icon name="domain" style={style} />
+  </HStack>`,...(r=(a=n.parameters)==null?void 0:a.docs)==null?void 0:r.source}}};const H=["Default"];export{n as Default,H as __namedExportsOrder,h as default};
